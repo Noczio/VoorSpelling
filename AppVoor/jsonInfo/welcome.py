1cad8c41121010a95a347f2f7752e81217ff89c8
nocz <devnocz@gmail.com>
2020-09-28 13:15:16 -0500

    file_name reformat

--- AppVoor/jsonInfo/welcome.py
+++ AppVoor/jsonInfo/welcome.py
@@ -6,8 +6,7 @@ from jsonInfo.random_generator import get_random_number_range_int
 
 class WelcomeMessage(ABCJson):
 
-    def __init__(self):
-        file_name = "welcomeMessage.json"
+    def __init__(self, file_name="welcomeMessage.json"):
         super().__init__()
         self.load_file(file_name)
 
@@ -26,6 +25,9 @@ class WelcomeMessenger:
         random_start = 0
         random_end = len(self._json_message.get_file_data())
         self._random_index = get_random_number_range_int(random_start, random_end, 1)
+
+    def __str__(self):
+        return self.get_welcome_message()
 
     def get_welcome_message(self) -> str:
         author, quote = self._json_message.get_data(self._random_index)
