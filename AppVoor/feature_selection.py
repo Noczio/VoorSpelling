b9dfd09f9a6e0b68aa89b8280eb8f4a000e9e25c
nocz <devnocz@gmail.com>
2020-11-07 22:54:28 -0500

    feature selection works. There are warnings tho

--- AppVoor/feature_selection.py
+++ AppVoor/feature_selection.py
@@ -6,6 +6,9 @@ import numpy as np
 
 from sklearn.feature_selection import SelectFromModel, RFE
 
+from jsonInfo.json_to_data import JSONMessage
+from jsonInfo.metrics import CVMetrics
+from score import CVScore, CVModelScore
 
 NpArray = np.ndarray
 DataFrame = pd.DataFrame
@@ -19,18 +22,97 @@ class FeatureSelection(ABC):
 
 
 class ForwardFeatureSelection(FeatureSelection):
+    _cv_score: CVModelScore
 
     def _first_iteration(self, x: DataFrame, y: NpArray, model: Any) -> tuple:
-        pass
+        score_lst = []  # empty list to store score values
+        # iterate over all features
+        for i in range(len(x.columns)):
+            # in each iteration get the column
+            k = x.columns[i]
+            # create a temp dataframe with the selected column
+            temp_x = x[[k]]
+            # get its score in a cv and append that values to the score_lst
+            score = self._get_cv_score(temp_x, y, model)
+            score_lst.append(score)
 
-    def _else_iteration(self, x: DataFrame, y: NpArray, model: Any) -> tuple:
-        pass
+        # get the max score from the score_lst
+        max_score = max(score_lst)
+        # get the index of all score with max score
+        max_score_index = [i for i, j in enumerate(score_lst) if j == max_score]
+        # get the top score from the max score index list
+        top_score_index = max_score_index[0]
+        # get feature name using top_score_index from original x dataframe
+        new_feature = x.columns[top_score_index]
+        # create a new dataframe with the winning feature
+        new_best_x = x[new_feature]
+        # drop the winning feature from the original dataframe and then store it into a new variable
+        new_x = x.drop(new_feature, axis=1)
+        # finally return the best feature dataframe, the new x without that feature and the max score of this iteration
+        return new_best_x, new_x, max_score
+
+    def _else_iteration(self, best_x: DataFrame, x: DataFrame, y: NpArray, model, actual_score: float) -> tuple:
+        # first check x len. this variable will become smaller and smaller over time
+        new_x_length = len(x.columns)
+        # if there are columns in the x dataframe then do the following process
+        if new_x_length > 0:
+            score_lst = []  # empty list to store score values
+            # iterate over all features from x dataframe
+            for i in range(new_x_length):
+                # in each iteration get the column
+                k = x.columns[i]
+                # create a temp dataframe with the selected column
+                temp_x = x[[k]]
+                temp_new_x = pd.concat([best_x, temp_x], axis=1, ignore_index=True)
+                # get its score in a cv and append that values to the score_lst
+                score = self._get_cv_score(temp_new_x, y, model)
+                score_lst.append(score)
+
+            # get the max score from the score_lst once the for loop has ended
+            max_score = max(score_lst)  # best score
+            # check if this iteration was worth it. If not then break the recursive method
+            if max_score < actual_score:
+                return best_x, actual_score
+
+            # get the index of all score with max score
+            max_score_index = [i for i, j in enumerate(score_lst) if j == max_score]
+            # get the top score from the max score index list
+            top_score_index = max_score_index[0]
+            # get feature name using top_score_index from original x dataframe
+            new_feature = x.columns[top_score_index]
+            # create a new dataframe with the winning feature
+            temp_x = x[new_feature]
+            # create a variable to store the last best dataframe with this new best feature group
+            new_best_x = pd.concat([best_x, temp_x], axis=1)
+            # drop the winning feature from the x dataframe and then store it into a new variable
+            new_x = x.drop(new_feature, axis=1)
+            # call the recursive function all over again until the condition is met
+            return self._else_iteration(new_best_x, new_x, y, model, max_score)
+
+        # x dataframe is now empty, return best x dataframe and its score
+        # this is bad scenario, because it iterated all features and there was not an improvement
+        return best_x, actual_score
+
+    def set_cv_score(self, cv_metrics: JSONMessage):
+        # create a CVScore object
+        self._cv_score = CVScore(cv_metrics)
 
     def _get_cv_score(self, x: DataFrame, y: NpArray, model: Any) -> float:
-        pass
+        # get score using the object and the method parameters and the return it
+        score = self._cv_score.get_score(x, y, model, "roc_auc", 10)
+        return score
 
     def select_features(self, x: DataFrame, y: NpArray, model: Any) -> DataFrame:
-        pass
+        _, initial_y_shape = x.shape  # original column len for evaluation
+        # if x only has 1 column then return original dataframe
+        if initial_y_shape == 1:
+            return x
+        else:
+            # else if x has more than 1 column
+            f_best_x, new_x, f_score = self._first_iteration(x, y, model)
+            # call recursive function and then return best x
+            best_x, best_score = self._else_iteration(f_best_x, new_x, y, model, f_score)
+            return best_x
 
 
 class SFMFeatureSelection(FeatureSelection):
--- AppVoor/tests/feature_selection_test.py
+++ AppVoor/tests/feature_selection_test.py
@@ -1,6 +1,7 @@
 import unittest
 
-from feature_selection import SFMFeatureSelection, RFEFeatureSelection
+from feature_selection import SFMFeatureSelection, RFEFeatureSelection, ForwardFeatureSelection
+from jsonInfo.metrics import CVMetrics
 from load_data import LoaderCreator
 from split_data import SplitterReturner
 
@@ -10,7 +11,7 @@ from sklearn.svm import LinearSVC
 class MyTestCase(unittest.TestCase):
     _loader_creator = LoaderCreator.get_instance()
 
-    def test_molecules_has_fewer_features(self):
+    def test_molecules_has_fewer_features_with_sfm(self):
         # load molecules.csv from disk
         folder_name = "datasets"
         file_name = "molecules.csv"
@@ -35,7 +36,7 @@ class MyTestCase(unittest.TestCase):
         # this should be True
         self.assertTrue(is_fewer_than_original)
 
-    def test_diabetes_has_fewer_features(self):
+    def test_diabetes_has_fewer_features_with_rfe(self):
         # load molecules.csv from disk
         folder_name = "datasets"
         file_name = "diabetes.csv"
@@ -47,6 +48,29 @@ class MyTestCase(unittest.TestCase):
         x, y = SplitterReturner.split_x_y_from_df(df)
         _, len_original_y = x.shape
         fs = RFEFeatureSelection()
+        # create a simple LinearSVC
+        clf = LinearSVC(random_state=0, dual=True)
+        # get new_x with new features
+        new_x = fs.select_features(x, y, clf)
+        _, len_new_y = new_x.shape
+        # does it have fewer features?
+        is_fewer_than_original: bool = True if len_new_y < len_original_y else False
+        # this should be True
+        self.assertTrue(is_fewer_than_original)
+
+    def test_diabetes_has_fewer_features_with_ffs(self):
+        # load molecules.csv from disk
+        folder_name = "datasets"
+        file_name = "diabetes.csv"
+        test_full_path = ".\\..\\" + folder_name + "\\" + file_name
+        # get dataframe using LoaderCreator
+        csv_type = self._loader_creator.create_loader(test_full_path, "csv")
+        df = csv_type.get_file_transformed()
+        # get x and y from SplitterReturner
+        x, y = SplitterReturner.split_x_y_from_df(df)
+        _, len_original_y = x.shape
+        fs = ForwardFeatureSelection()
+        fs.set_cv_score(CVMetrics(".\\..\\jsonInfo\\CVMetrics.json", list))
         # create a simple LinearSVC
         clf = LinearSVC(random_state=0, dual=True)
         # get new_x with new features
