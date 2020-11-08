d2ad4a2dfd8963358a8ee1cc1940ffc10a816b85
nocz <devnocz@gmail.com>
2020-11-07 21:48:32 -0500

    feature selection goes well

--- AppVoor/feature_selection.py
+++ AppVoor/feature_selection.py
@@ -4,6 +4,7 @@ from typing import Any
 import pandas as pd
 import numpy as np
 
+from jsonInfo.json_to_data import JSONMessage
 from score import CVModelScore, CVScore
 from sklearn.feature_selection import SelectFromModel, RFE
 
@@ -14,93 +15,29 @@ DataFrame = pd.DataFrame
 
 class FeatureSelection(ABC):
 
-    def __init__(self, score_type: str, n_folds_validation: int) -> None:
-        self._score_type = score_type
-        self._n_folds_validation = n_folds_validation
-
     @abstractmethod
     def select_features(self, x: DataFrame, y: NpArray, model: Any) -> DataFrame:
         pass
-
-
-class OwnFeatureSelection(FeatureSelection):
-
-    _cv_score: CVModelScore = CVScore(file_path="", data_type=list)
-
-    def _first_iteration(self, x: DataFrame, y: NpArray, model: Any) -> tuple:
-        score_lst = []
-        for i in range(len(x.columns)):
-            k = x.columns[i]
-            temp_x = x[[k]]
-            score = self._cv_score.get_score(temp_x, y, model, self._score_type, self._n_folds_validation)
-            score_lst.append(score)
-
-        max_score = max(score_lst)  # best score
-        max_score_index = [i for i, j in enumerate(score_lst) if j == max_score]  # index with best score
-        top_score_index = max_score_index[0]
-        new_feature = x.columns[top_score_index]
-        new_best_x = x[new_feature]
-        new_x = x.drop(new_feature, axis=1)
-
-        return new_best_x, new_x, max_score
-
-    def _else_iteration(self, best_x: DataFrame, x: DataFrame, y: NpArray, model: Any, actual_score: float) -> tuple:
-        new_x_length = len(x.columns)
-        if new_x_length > 0:
-            score_lst = []
-            for i in range(new_x_length):
-                k = x.columns[i]
-                temp_x = x[[k]]
-                temp_new_x = pd.concat([best_x, temp_x], axis=1, ignore_index=True)
-                score = self._cv_score.get_score(temp_new_x, y, model, self._score_type, self._n_folds_validation)
-                score_lst.append(score)
-
-            max_score = max(score_lst)  # best score
-
-            if max_score < actual_score:
-                return best_x, actual_score  # break condition, recursive function
-
-            max_score_index = [i for i, j in enumerate(score_lst) if j == max_score]  # index with best score
-            top_score_index = max_score_index[0]
-
-            new_feature = x.columns[top_score_index]
-            temp_x = x[new_feature]
-            new_best_x = pd.concat([best_x, temp_x], axis=1)
-            new_x = x.drop(new_feature, axis=1)
-
-            return self._else_iteration(new_best_x, new_x, y, model, max_score)
-
-        return best_x, actual_score
-
-    def select_features(self, x: DataFrame, y: NpArray, model: Any) -> DataFrame:
-        _, initial_y_shape = x.shape
-        # if x only has 1 column then, return dataframe with its score
-        if initial_y_shape == 1:
-            # score = self._cv_score.get_score(x, y, model, self._score_type, self._n_folds_validation)
-            return x
-        # else if x has more than 1 column
-        f_best_x, new_x, f_score = self._first_iteration(x, y, model)
-        best_x, best_score = self._else_iteration(f_best_x, new_x, y, model, f_score)
-
-        return best_x
 
 
 class SFMFeatureSelection(FeatureSelection):
 
     def select_features(self, x: DataFrame, y: NpArray, model: Any) -> DataFrame:
         clf = model.fit(x, y)
-        model = SelectFromModel(clf, prefit=True)
-        feature_idx = model.get_support()
-        feature_name = x.columns[feature_idx]
-        return x[feature_name]
+        sfm = SelectFromModel(clf, prefit=True)
+        _ = sfm.transform(x)
+        features = x.columns[sfm.get_support()]
+        transformed_x = x[features]
+        return transformed_x
 
 
-class SFSFeatureSelection(FeatureSelection):
+class RFEFeatureSelection(FeatureSelection):
 
     def select_features(self, x: DataFrame, y: NpArray, model: Any) -> DataFrame:
         clf = model
-        model = RFE(clf)
-        model.fit(x, y)
-        feature_idx = model.get_support()
-        feature_name = x.columns[feature_idx]
-        return x[feature_name]
+        rfe = RFE(clf)
+        rfe.fit(x, y)
+        _ = rfe.transform(x)
+        features = x.columns[rfe.get_support()]
+        transformed_x = x[features]
+        return transformed_x
---
+++ AppVoor/tests/feature_selection_test.py
@@ -0,0 +1,62 @@
+import unittest
+
+from feature_selection import SFMFeatureSelection, RFEFeatureSelection
+from load_data import LoaderCreator
+from split_data import SplitterReturner
+
+from sklearn.svm import LinearSVC
+
+
+class MyTestCase(unittest.TestCase):
+    _loader_creator = LoaderCreator.get_instance()
+
+    def test_molecules_has_fewer_features(self):
+        # load molecules.csv from disk
+        folder_name = "datasets"
+        file_name = "molecules.csv"
+        test_full_path = ".\\..\\" + folder_name + "\\" + file_name
+        # get dataframe using LoaderCreator
+        tsv_type = self._loader_creator.create_loader(test_full_path, "tsv")
+        df = tsv_type.get_file_transformed()
+        # get x and y from SplitterReturner
+        x, y = SplitterReturner.split_x_y_from_df(df)
+        x = x.drop(["m_name"], axis=1)
+        _, len_original_y = x.shape
+        # create a CVMetrics object and then use it as parameter for OwnFeatureSelection
+        # cv_metrics = CVMetrics(file_path=".\\..\\jsonInfo\\CVMetrics.json", data_type=list)
+        fs = SFMFeatureSelection()
+        # create a simple LinearSVC
+        clf = LinearSVC(C=3, tol=0.0001, random_state=0, dual=True)
+        # get new_x with new features
+        new_x = fs.select_features(x, y, clf)
+        _, len_new_y = new_x.shape
+        # does it have fewer features?
+        is_fewer_than_original: bool = True if len_new_y < len_original_y else False
+        # this should be True
+        self.assertTrue(is_fewer_than_original)
+
+    def test_diabetes_has_fewer_features(self):
+        # load molecules.csv from disk
+        folder_name = "datasets"
+        file_name = "diabetes.csv"
+        test_full_path = ".\\..\\" + folder_name + "\\" + file_name
+        # get dataframe using LoaderCreator
+        csv_type = self._loader_creator.create_loader(test_full_path, "csv")
+        df = csv_type.get_file_transformed()
+        # get x and y from SplitterReturner
+        x, y = SplitterReturner.split_x_y_from_df(df)
+        _, len_original_y = x.shape
+        fs = RFEFeatureSelection()
+        # create a simple LinearSVC
+        clf = LinearSVC(random_state=0, dual=True)
+        # get new_x with new features
+        new_x = fs.select_features(x, y, clf)
+        _, len_new_y = new_x.shape
+        # does it have fewer features?
+        is_fewer_than_original: bool = True if len_new_y < len_original_y else False
+        # this should be True
+        self.assertTrue(is_fewer_than_original)
+
+
+if __name__ == '__main__':
+    unittest.main()
