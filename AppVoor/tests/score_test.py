6fa7472b12da3dc49d909246fe013942b17ef7f1
nocz <devnocz@gmail.com>
2020-11-17 20:49:14 -0500

    new tests for score and is data. All works so far

--- AppVoor/tests/score_test.py
+++ AppVoor/tests/score_test.py
@@ -1,31 +1,14 @@
 import unittest
 
+from estimator_creation import EstimatorCreator
 from load_data import LoaderCreator
 from score import CVScore
 from split_data import SplitterReturner
-from sklearn.svm import LinearSVC
 
 
 class MyTestCase(unittest.TestCase):
     _loader_creator = LoaderCreator.get_instance()
-
-    def test_score_is_more_than_zero(self):
-        # path to diabetes.csv file in project
-        path = ".\\..\\datasets\\diabetes.csv"
-        # get df with loader creator
-        csv_type = self._loader_creator.create_loader(path, "CSV")
-        df = csv_type.get_file_transformed()
-        # split df into x and y
-        splitter = SplitterReturner()
-        x, y = splitter.split_x_y_from_df(df)
-        # create a CVScore object with its path and data type
-        cv_score = CVScore()
-        # create a simple linearSVC estimator
-        model = LinearSVC(random_state=0, dual=False)
-        # get score from a linearSVC estimator with roc_auc score and 10 folds
-        score = cv_score.get_score(x, y, model, "roc_auc", 10)
-        is_greater_than_zero: bool = True if score > 0 else False
-        self.assertTrue(is_greater_than_zero)
+    _estimator_creator = EstimatorCreator.get_instance()
 
     def test_score_type_raises_ValueError(self):
         # path to diabetes.csv file in project
@@ -39,7 +22,8 @@ class MyTestCase(unittest.TestCase):
         # create a CVScore object with its path and data type
         cv_score = CVScore()
         # create a simple linearSVC estimator
-        model = LinearSVC(random_state=0, dual=False)
+        model = self._estimator_creator.create_estimator("LSVC")
+        model.set_params(dual=False, random_state=0)
         with self.assertRaises(ValueError):
             # get score from a linearSVC estimator with roc_auc score and 10 folds
             _ = cv_score.get_score(x, y, model, "roc", 10)
@@ -56,7 +40,8 @@ class MyTestCase(unittest.TestCase):
         # create a CVScore object with its path and data type
         cv_score = CVScore()
         # create a simple linearSVC estimator
-        model = LinearSVC(random_state=0, dual=False)
+        model = self._estimator_creator.create_estimator("LSVC")
+        model.set_params(dual=False, random_state=0)
         with self.assertRaises(ValueError):
             # get score from a linearSVC estimator with roc_auc score and 10 folds
             _ = cv_score.get_score(x, y, model, "roc_auc", 2)
@@ -73,10 +58,40 @@ class MyTestCase(unittest.TestCase):
         # create a CVScore object with its path and data type
         cv_score = CVScore()
         # create a simple linearSVC estimator
-        model = LinearSVC(random_state=0, dual=False)
+        model = self._estimator_creator.create_estimator("LSVC")
+        model.set_params(dual=False, random_state=0)
         with self.assertRaises(ValueError):
             # get score from a linearSVC estimator with roc_auc score and 10 folds
             _ = cv_score.get_score(x, y, model, "roc", 2)
+
+    def test_cv_score_is_more_than_zero_with_LSVC_SVC_KNN_GNB(self):
+        # path to diabetes.csv file in project
+        path = ".\\..\\datasets\\diabetes.csv"
+        # get df with loader creator
+        csv_type = self._loader_creator.create_loader(path, "CSV")
+        df = csv_type.get_file_transformed()
+        # split df into x and y
+        splitter = SplitterReturner()
+        x, y = splitter.split_x_y_from_df(df)
+        # create a CVScore object with its path and data type
+        cv_score = CVScore()
+        # create a simple a svc, knn and gnb estimator
+        model_1 = self._estimator_creator.create_estimator("SVC")
+        model_2 = self._estimator_creator.create_estimator("KNN")
+        model_3 = self._estimator_creator.create_estimator("GNB")
+        model_4 = self._estimator_creator.create_estimator("LSVC")
+        estimators = [model_1, model_2, model_3, model_4.set_params(dual=False)]
+        # get score from a linearSVC estimator with accuracy score and 5folds
+        bol_results = []
+        for clf in estimators:
+            score = cv_score.get_score(x, y, clf, "accuracy", 5)
+            print(clf.__class__.__name__, "score is:", score)
+            is_greater_than_zero: bool = True if score > 0 else False
+            bol_results.append(is_greater_than_zero)
+        # any will return True if there's any truth value in the iterable.
+        answer = any(bol_results)
+        # all of this should be true
+        self.assertTrue(answer)
 
 
 if __name__ == '__main__':
