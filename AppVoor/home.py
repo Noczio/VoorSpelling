51158f10c1772dd664b9d1d7c9f326e942e51412
Nocz <devnocz@gmail.com>
2020-12-07 22:05:36 -0500

    home fixed to center

--- AppVoor/home.py
+++ AppVoor/home.py
@@ -10,12 +10,20 @@ class Window(QMainWindow):
     def __init__(self, window: str):
         super().__init__()
         uic.loadUi(window, self)
+        self.center_screen()
         self.change_message()
 
     def change_message(self):
         messenger = WelcomeMessenger(file_path=".\\jsonInfo\\welcomeMessage.json")
         text = str(messenger)
         self.lbl_phrase.setText(text)
+
+    def center_screen(self):
+        frame_gm = self.frameGeometry()
+        screen = QApplication.desktop().screenNumber(QApplication.desktop().cursor().pos())
+        center_point = QApplication.desktop().screenGeometry(screen).center()
+        frame_gm.moveCenter(center_point)
+        self.move(frame_gm.topLeft())
 
 
 if __name__ == "__main__":
