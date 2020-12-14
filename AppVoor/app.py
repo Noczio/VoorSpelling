4981c3b0b2c203b04d563f79f6be370f745ebb96
Nocz <devnocz@gmail.com>
2020-12-14 17:44:24 -0500

    better pop up implementation and prediction type first part implementation

--- AppVoor/app.py
+++ AppVoor/app.py
@@ -1,41 +1,66 @@
 import sys
 
-from PyQt5 import QtWidgets
-from PyQt5 import uic
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
 
 from abc import ABC, abstractmethod
 
 from jsonInfo.welcome import WelcomeMessenger
 from jsonInfo.help import HelpMessage
+from load_data import LoaderCreator
+from model_creation import SBSModelCreator
+from estimator_creation import EstimatorCreator
+from feature_selection import FeatureSelectorCreator
+from parameter_search import ParameterSearchCreator
+from auto_ml import JarAutoML, AutoExecutioner
+from split_data import SplitterReturner
 import forms.resources
 
 
 class Window(QMainWindow):
+
     def __init__(self, window: str, help_message_path: str = ".\\jsonInfo\\helpMessage.json") -> None:
         super().__init__()
         uic.loadUi(window, self)
         self._help_message = HelpMessage(file_path=help_message_path)
 
-    def next(self) -> None:
-        pass
-
-    def back(self) -> None:
-        pass
-
-    def show_info(self, key: str):
-        title, body, example, url = self._help_message[key]
+    def general_info_pop_up(self, title: str, body: str, additional: str = "",
+                       pop_up_type: any = QMessageBox.Information, buttons: any = QMessageBox.Ok) -> None:
+        # set font as QFont object
+        font = QFont()
+        font.setFamily("MS Shell")
+        font.setPointSize(11)
+        # create a QMessageBox object and then set its values
         msg = QMessageBox()
         msg.setWindowTitle(title)
-        if example == "":
-            msg.setText(body)
-        else:
-            msg.setText(body+"\n\n"+"Ejemplo:"+" "+example)
+        msg.setText(body)
+
+        if additional is not "":
+            msg.setInformativeText(additional)
+
+        msg.setIcon(pop_up_type)
+        msg.setStandardButtons(buttons)
+        msg.setFont(font)
+
+        _ = msg.exec_()  # this will show the messagebox
+
+    def useful_info_pop_up(self, key: str) -> None:
+        # get help message info from HelpMessage object
+        title, body, example, url = self._help_message[key]
+        if example is not "":
+            body = body + "\n\n" + "Ejemplo:" + "\n\n" + example
         if url is not "":
-            msg.setInformativeText("\n\n"+"Para más información visitar:"+" "+url)
-        msg.setIcon(QMessageBox.Information)
-        msg.setStandardButtons(QMessageBox.Ok)
-        _ = msg.exec_()  # this will show our messagebox
+            url = "Para más información vistiar:" + " " + url
+        # call general_info_pop_up with useful_info_pop_up info
+        self.general_info_pop_up(title, body, additional=url)
+
+    def next(self, *args, **kwargs) -> None:
+        pass
+
+    def back(self, *args, **kwargs) -> None:
+        pass
 
 
 class HomeWindow(Window):
@@ -50,7 +75,7 @@ class HomeWindow(Window):
         text = str(messenger)
         self.lbl_description.setText(text)
 
-    def center_window(self):
+    def center_window(self) -> None:
         frame_gm = self.frameGeometry()
         screen = QApplication.desktop().screenNumber(QApplication.desktop().cursor().pos())
         center_point = QApplication.desktop().screenGeometry(screen).center()
@@ -66,13 +91,19 @@ class HomeWindow(Window):
 
 class DataSetWindow(Window):
 
-    def __init__(self, window: str, help_message_path: str = ".\\jsonInfo\\helpMessage.json"):
+    def __init__(self, window: str, help_message_path: str = ".\\jsonInfo\\helpMessage.json") -> None:
         super().__init__(window, help_message_path)
         self.btn_back.clicked.connect(self.back)
-        self.btn_info_data_type.clicked.connect(self.info_data_type)
+        self.btn_next.clicked.connect(self.next)
+        self.btn_info_data_type.clicked.connect(lambda: self.useful_info_pop_up("file_separation"))
+
+        self._file_type = "CSV"
+        self._selected_data_path = ""
 
     def next(self) -> None:
-        # to do. Validate if data is a valid dataset and some option has been selected
+        # loader_creator = LoaderCreator.get_instance()
+        # loader = loader_creator.create_loader(selected_data, self._file_type)
+        # file_transformed = loader.get_file_transformed()
         next_form = MLTypeWindow(ui_window["model"])
         widget.addWidget(next_form)
         widget.removeWidget(widget.currentWidget())
@@ -84,22 +115,69 @@ class DataSetWindow(Window):
         widget.removeWidget(widget.currentWidget())
         widget.setCurrentIndex(widget.currentIndex())
 
-    def info_data_type(self):
-        self.show_info("file_separation")
+    def load_dataset(self):
+        pass
 
 
 class MLTypeWindow(Window):
 
-    def __init__(self, window: str, help_message_path: str = ".\\jsonInfo\\helpMessage.json"):
+    def __init__(self, window: str, help_message_path: str = ".\\jsonInfo\\helpMessage.json") -> None:
         super().__init__(window, help_message_path)
         self.btn_back.clicked.connect(self.back)
+        self.btn_info_automl.clicked.connect(lambda: self.useful_info_pop_up("auto_machine_learning"))
+        self.btn_info_sbsml.clicked.connect(lambda: self.useful_info_pop_up("step_by_step"))
+        self.btn_next.clicked.connect(self.next)
 
     def next(self) -> None:
         # to do. Validate if any of the radio buttons are selected and the go the next form
-        pass
+        if self.rbtn_sbsml.isChecked():
+            next_form = PredictionType(ui_window["prediction_type"])
+            widget.addWidget(next_form)
+            widget.removeWidget(widget.currentWidget())
+            widget.setCurrentIndex(widget.currentIndex())
+        else:
+            next_form = AutoLoad(ui_window["result_screen"])
+            widget.addWidget(next_form)
+            widget.removeWidget(widget.currentWidget())
+            widget.setCurrentIndex(widget.currentIndex())
 
     def back(self) -> None:
         last_form = DataSetWindow(ui_window["dataset"])
+        widget.addWidget(last_form)
+        widget.removeWidget(widget.currentWidget())
+        widget.setCurrentIndex(widget.currentIndex())
+
+
+class AutoLoad(Window):
+
+    def __init__(self, window: str, help_message_path: str = ".\\jsonInfo\\helpMessage.json") -> None:
+        super().__init__(window, help_message_path)
+        self.lbl_cancel.mouseReleaseEvent = self.back
+
+    def back(self, event):
+        last_form = HomeWindow(ui_window["home"])
+        widget.addWidget(last_form)
+        widget.removeWidget(widget.currentWidget())
+        widget.setCurrentIndex(widget.currentIndex())
+
+
+class StepByStepLoad(Window):
+
+    def __init__(self, window: str, help_message_path: str = ".\\jsonInfo\\helpMessage.json") -> None:
+        super().__init__(window, help_message_path)
+
+
+class PredictionType(Window):
+
+    def __init__(self, window: str, help_message_path: str = ".\\jsonInfo\\helpMessage.json") -> None:
+        super().__init__(window, help_message_path)
+        self.btn_back.clicked.connect(self.back)
+        self.btn_info_Classification.clicked.connect(lambda: self.useful_info_pop_up("classification"))
+        self.btn_info_Regression.clicked.connect(lambda: self.useful_info_pop_up("regression"))
+        self.btn_info_Clustering.clicked.connect(lambda: self.useful_info_pop_up("clustering"))
+
+    def back(self) -> None:
+        last_form = MLTypeWindow(ui_window["model"])
         widget.addWidget(last_form)
         widget.removeWidget(widget.currentWidget())
         widget.setCurrentIndex(widget.currentIndex())
