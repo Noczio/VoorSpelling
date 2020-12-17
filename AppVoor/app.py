3df5aa7867c902d3820a3201903ef489b825418f
Nocz <devnocz@gmail.com>
2020-12-17 11:41:28 -0500

    help message fix and app implementations

--- AppVoor/app.py
+++ AppVoor/app.py
@@ -1,12 +1,10 @@
 import sys
 
 from PyQt5 import QtWidgets, uic
-from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
-from abc import ABC, abstractmethod
-
+from pop_up import PopUp, InfoPopUp, WarningPopUp
 from jsonInfo.welcome import WelcomeMessenger
 from jsonInfo.help import HelpMessage
 from load_data import LoaderCreator
@@ -26,26 +24,6 @@ class Window(QMainWindow):
         uic.loadUi(window, self)
         self._help_message = HelpMessage(file_path=help_message_path)
 
-    def general_info_pop_up(self, title: str, body: str, additional: str = "",
-                       pop_up_type: any = QMessageBox.Information, buttons: any = QMessageBox.Ok) -> None:
-        # set font as QFont object
-        font = QFont()
-        font.setFamily("MS Shell")
-        font.setPointSize(11)
-        # create a QMessageBox object and then set its values
-        msg = QMessageBox()
-        msg.setWindowTitle(title)
-        msg.setText(body)
-
-        if additional is not "":
-            msg.setInformativeText(additional)
-
-        msg.setIcon(pop_up_type)
-        msg.setStandardButtons(buttons)
-        msg.setFont(font)
-
-        _ = msg.exec_()  # this will show the messagebox
-
     def useful_info_pop_up(self, key: str) -> None:
         # get help message info from HelpMessage object
         title, body, example, url = self._help_message[key]
@@ -54,7 +32,8 @@ class Window(QMainWindow):
         if url is not "":
             url = "Para más información vistiar:" + " " + url
         # call general_info_pop_up with useful_info_pop_up info
-        self.general_info_pop_up(title, body, additional=url)
+        pop_up: PopUp = InfoPopUp()
+        pop_up.open_pop_up(title, body, url)
 
     def next(self, *args, **kwargs) -> None:
         pass
@@ -96,11 +75,11 @@ class DataSetWindow(Window):
         self.btn_back.clicked.connect(self.back)
         self.btn_next.clicked.connect(self.next)
         self.btn_info_data_type.clicked.connect(lambda: self.useful_info_pop_up("file_separation"))
-
         self._file_type = "CSV"
         self._selected_data_path = ""
 
     def next(self) -> None:
+        # to do. Validate if info is correct. If it is not don't continue and show pop up
         # loader_creator = LoaderCreator.get_instance()
         # loader = loader_creator.create_loader(selected_data, self._file_type)
         # file_transformed = loader.get_file_transformed()
@@ -116,6 +95,7 @@ class DataSetWindow(Window):
         widget.setCurrentIndex(widget.currentIndex())
 
     def load_dataset(self):
+        # to do. Open a file browser a store path into _selected_data_path
         pass
 
 
@@ -129,7 +109,6 @@ class MLTypeWindow(Window):
         self.btn_next.clicked.connect(self.next)
 
     def next(self) -> None:
-        # to do. Validate if any of the radio buttons are selected and the go the next form
         if self.rbtn_sbsml.isChecked():
             next_form = PredictionType(ui_window["prediction_type"])
             widget.addWidget(next_form)
@@ -155,16 +134,33 @@ class AutoLoad(Window):
         self.lbl_cancel.mouseReleaseEvent = self.back
 
     def back(self, event):
-        last_form = HomeWindow(ui_window["home"])
-        widget.addWidget(last_form)
-        widget.removeWidget(widget.currentWidget())
-        widget.setCurrentIndex(widget.currentIndex())
+        pop_up: PopUp = WarningPopUp()
+        title = "Cancelar entrenamiento"
+        body = "¿Estas seguro que deseas cancelar el entrenamiento?. Toda la información será eliminada."
+        answer = pop_up.open_pop_up(title, body, "")
+        if answer:
+            last_form = HomeWindow(ui_window["home"])
+            widget.addWidget(last_form)
+            widget.removeWidget(widget.currentWidget())
+            widget.setCurrentIndex(widget.currentIndex())
 
 
 class StepByStepLoad(Window):
 
     def __init__(self, window: str, help_message_path: str = ".\\jsonInfo\\helpMessage.json") -> None:
         super().__init__(window, help_message_path)
+        self.lbl_cancel.mouseReleaseEvent = self.back
+
+    def back(self, event):
+        pop_up: PopUp = WarningPopUp()
+        title = "Cancelar entrenamiento"
+        body = "¿Estas seguro que deseas cancelar el entrenamiento?. Toda la información será eliminada."
+        answer = pop_up.open_pop_up(title, body, "")
+        if answer:
+            last_form = HomeWindow(ui_window["home"])
+            widget.addWidget(last_form)
+            widget.removeWidget(widget.currentWidget())
+            widget.setCurrentIndex(widget.currentIndex())
 
 
 class PredictionType(Window):
@@ -175,9 +171,61 @@ class PredictionType(Window):
         self.btn_info_Classification.clicked.connect(lambda: self.useful_info_pop_up("classification"))
         self.btn_info_Regression.clicked.connect(lambda: self.useful_info_pop_up("regression"))
         self.btn_info_Clustering.clicked.connect(lambda: self.useful_info_pop_up("clustering"))
+        self.btn_Classification.clicked.connect(lambda: self.next("classification"))
+        self.btn_Regression.clicked.connect(lambda: self.next("regression"))
+        self.btn_Clustering.clicked.connect(lambda: self.next("clustering"))
+
+    def next(self, event) -> None:
+        possibilities = {"classification": ClassificationSelection(ui_window["classification"]),
+                         "regression": RegressionSelection(ui_window["regression"]),
+                         "clustering": ClusteringSelection(ui_window["clustering"])}
+
+        next_form = possibilities[event]
+        widget.addWidget(next_form)
+        widget.removeWidget(widget.currentWidget())
+        widget.setCurrentIndex(widget.currentIndex())
 
     def back(self) -> None:
         last_form = MLTypeWindow(ui_window["model"])
+        widget.addWidget(last_form)
+        widget.removeWidget(widget.currentWidget())
+        widget.setCurrentIndex(widget.currentIndex())
+
+
+class ClassificationSelection(Window):
+
+    def __init__(self, window: str, help_message_path: str = ".\\jsonInfo\\helpMessage.json") -> None:
+        super().__init__(window, help_message_path)
+        self.btn_back.clicked.connect(self.back)
+
+    def back(self) -> None:
+        last_form = PredictionType(ui_window["prediction_type"])
+        widget.addWidget(last_form)
+        widget.removeWidget(widget.currentWidget())
+        widget.setCurrentIndex(widget.currentIndex())
+
+
+class RegressionSelection(Window):
+
+    def __init__(self, window: str, help_message_path: str = ".\\jsonInfo\\helpMessage.json") -> None:
+        super().__init__(window, help_message_path)
+        self.btn_back.clicked.connect(self.back)
+
+    def back(self) -> None:
+        last_form = PredictionType(ui_window["prediction_type"])
+        widget.addWidget(last_form)
+        widget.removeWidget(widget.currentWidget())
+        widget.setCurrentIndex(widget.currentIndex())
+
+
+class ClusteringSelection(Window):
+
+    def __init__(self, window: str, help_message_path: str = ".\\jsonInfo\\helpMessage.json") -> None:
+        super().__init__(window, help_message_path)
+        self.btn_back.clicked.connect(self.back)
+
+    def back(self) -> None:
+        last_form = PredictionType(ui_window["prediction_type"])
         widget.addWidget(last_form)
         widget.removeWidget(widget.currentWidget())
         widget.setCurrentIndex(widget.currentIndex())
