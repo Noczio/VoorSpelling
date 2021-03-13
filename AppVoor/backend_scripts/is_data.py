fd35fecd645ae6753397dcb4cfa013ef0c53a93c
Nocz <devnocz@gmail.com>
2021-03-13 13:07:50 -0500

    v 1.08 corrupted files no longer are considered a dataframe

--- AppVoor/backend_scripts/is_data.py
+++ AppVoor/backend_scripts/is_data.py
@@ -25,13 +25,28 @@ class PdData(ValidData):
 
     def data_is_valid(self, data: Any, expected: Any) -> bool:
         # this method is supposed to be used to valid if a dataframe has enough samples and features to train
-        min_data = 100
         if isinstance(data, expected):
             # x_shape is number of rows (samples) and y_shape is number of columns (features)
             x_shape, y_shape = data.shape
-            if x_shape <= min_data or y_shape <= 1:
-                return False
-            return True
+            min_x_data, min_y_data = 100, 2
+            if y_shape >= min_y_data and x_shape >= min_x_data:
+                not_valid = ("", ",", ";", "\t")
+                columns = [value for value in data]
+                # if all columns seem ok then proceed to check for rows
+                for col in columns:
+                    for col_char in str(col):
+                        if col_char in not_valid:
+                            return False
+                """for every row, for every column: check if current data has a not valid info. if true 
+                return a false (not a valid data)"""
+                rows = [0, 1, min_x_data - 1, min_x_data, min_x_data + 1, x_shape - 2, x_shape - 1]
+                for i in rows:
+                    for j in columns:
+                        for row_char in str(data.at[i, j]):
+                            if row_char in not_valid:
+                                return False
+                return True
+            return False
         return False
 
 
